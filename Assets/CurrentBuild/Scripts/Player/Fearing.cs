395def6f6b721018ec61e0213479db7eff0e4260
Rachel Alberts <rd.alberts@outlook.com>
2018-06-15 14:30:53 +0200

    Created the singleton

--- Assets/CurrentBuild/Scripts/Player/Fearing.cs
+++ Assets/CurrentBuild/Scripts/Player/Fearing.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Fearing : MonoBehaviour {
+public class Fearing : MonoBehaviour
+{
+
+    public static Fearing instance = null;
     public List<GameObject> residents = new List<GameObject>();
 
     public List<GameObject>  interactions = new List<GameObject>();
@@ -25,8 +28,27 @@ public class Fearing : MonoBehaviour {
         }
     }
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
 
         // checkt alle combinatiess van interacties en residents en kijkt welke bang gemaakt moeten worden.
         foreach (GameObject interaction in interactions)
---
+++ Assets/CurrentBuild/Scripts/Player/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+
+    public GameObject fearing;
+
+	// Use this for initialization
+	void Awake () {
+	    //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+	    if (Fearing.instance == null)
+
+	        //Instantiate gameManager prefab
+	        Instantiate(fearing);
+    }
+	
+}
